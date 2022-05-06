9589458a7a19e48f13b715b90ba63c232efb9106
Anchal <anchal.mandavia@gmail.com>
2022-05-06 09:46:40 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -167,7 +167,36 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
 		Stack<BSTNode<T>> in = new Stack<BSTNode<T>>();
-		
+		 if (root == null)
+	            return;
+
+	     BSTNode<T> curr = root;
+	 
+	     // traverse the tree
+	     while (curr != null || in.size() > 0)
+	     {
+	 
+	         /* Reach the left most Node of the
+	         curr Node */
+	         while (curr !=  null)
+	         {
+	          /* place pointer to a tree node on
+	             the stack before traversing
+	             the node's left subtree */
+	             in.push(curr);
+	             curr = curr.leftChild;
+	         }
+	 
+	         /* Current must be NULL at this point */
+	         curr = in.pop();
+	 
+	         System.out.print(curr.data + " ");
+	 
+	         /* we have visited the node and its
+	            left subtree.  Now, it's right
+	            subtree's turn */
+	         curr = curr.rightChild;
+	        }
 		
 	}
 	
